f2b969a28597374f49c37120485564d147c2eb5c
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-06-12 16:45:24 +0200

    polish

---
+++ my-app/src/constants/index.ts
@@ -0,0 +1,60 @@
+import { MenuItemType } from '../form/RHFSelectInput';
+
+export const cupTypes: MenuItemType[] = [
+  {
+    key: 1,
+    value: 35,
+    menuItemLabel: '212ml',
+  },
+  {
+    key: 2,
+    value: 46,
+    menuItemLabel: '370ml',
+  },
+];
+
+export const cupPrice: MenuItemType[] = [
+  {
+    key: 1,
+    value: 350,
+    menuItemLabel: '212ml',
+  },
+  {
+    key: 2,
+    value: 550,
+    menuItemLabel: '370ml',
+  },
+];
+
+export const fruits: MenuItemType[] = [
+  {
+    key: 1,
+    value: 1,
+    menuItemLabel: 'Šljiva',
+  },
+  {
+    key: 2,
+    value: 2,
+    menuItemLabel: 'Kruška',
+  },
+  {
+    key: 3,
+    value: 3,
+    menuItemLabel: 'Narandža',
+  },
+  {
+    key: 4,
+    value: 4,
+    menuItemLabel: 'Jagoda',
+  },
+  {
+    key: 5,
+    value: 5,
+    menuItemLabel: 'Kupina',
+  },
+  {
+    key: 6,
+    value: 6,
+    menuItemLabel: 'Malina',
+  },
+];
--- my-app/src/form/index.tsx
+++ my-app/src/form/index.tsx
@@ -4,8 +4,9 @@ import { useForm } from 'react-hook-form';
 
 import FormattedPrice from '../utils/FormattedPrice';
 import FormProvider from './FormProvider';
-import RHFSelectInput, { MenuItemType } from './RHFSelectInput';
+import RHFSelectInput from './RHFSelectInput';
 import RHFTextInput from './RHFTextInput';
+import { cupPrice, cupTypes, fruits } from '../constants';
 
 type FormData = {
   numOfCups: number | string;
@@ -38,65 +39,6 @@ type FormData = {
   totalIncome: number | string;
   totalProfit: number | string;
 };
-
-const cupTypes: MenuItemType[] = [
-  {
-    key: 1,
-    value: 35,
-    menuItemLabel: '212ml',
-  },
-  {
-    key: 2,
-    value: 46,
-    menuItemLabel: '370ml',
-  },
-];
-
-const cupPrice: MenuItemType[] = [
-  {
-    key: 1,
-    value: 350,
-    menuItemLabel: '212ml',
-  },
-  {
-    key: 2,
-    value: 550,
-    menuItemLabel: '370ml',
-  },
-];
-
-const fruits: MenuItemType[] = [
-  {
-    key: 1,
-    value: 1,
-    menuItemLabel: 'Šljiva',
-  },
-  {
-    key: 2,
-    value: 2,
-    menuItemLabel: 'Kruška',
-  },
-  {
-    key: 3,
-    value: 3,
-    menuItemLabel: 'Narandža',
-  },
-  {
-    key: 4,
-    value: 4,
-    menuItemLabel: 'Jagoda',
-  },
-  {
-    key: 5,
-    value: 5,
-    menuItemLabel: 'Kupina',
-  },
-  {
-    key: 6,
-    value: 6,
-    menuItemLabel: 'Malina',
-  },
-];
 
 export default function Form() {
   const methods = useForm<FormData>({
