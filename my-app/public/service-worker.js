08b1d5962aa102412c1c572a92a43008cb5f0491
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-06-27 17:45:23 +0200

    service woker

--- my-app/public/service-worker.js
+++ my-app/public/service-worker.js
@@ -2,19 +2,17 @@
 
 const CACHE_NAME = 'my-pwa-cache-v1';
 const FILES_TO_CACHE = [
-  '/',
+  '/', // Make sure this resolves to index.html
   '/index.html',
   '/manifest.json',
   '/icon-192x192.png',
   '/icon-512x512.png',
-  // Add your build assets here if not hashed: e.g., '/static/js/main.js'
+  // Add paths to your built JS/CSS files if not hashed
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Install');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -22,35 +20,30 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => {
             console.log('[ServiceWorker] Removing old cache', key);
             return caches.delete(key);
-          }
-        })
-      )
-    )
+          })
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('[ServiceWorker] Fetch', event.request.url);
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        return response;
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then((cachedResponse) => {
+        // If not in cache, return the app shell (index.html)
+        return cachedResponse || caches.match('/');
       })
-      .catch(() => {
-        return caches.match(event.request).then((cachedResponse) => {
-          return cachedResponse || caches.match('/');
-        });
-      })
+    )
   );
 });
