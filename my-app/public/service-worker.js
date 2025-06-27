3da01799a6551fb3d1011c5a39f5be5530b9fbe9
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-06-27 17:19:48 +0200

    comment service worker

--- my-app/public/service-worker.js
+++ my-app/public/service-worker.js
@@ -1,13 +1,56 @@
 /* eslint-disable no-restricted-globals */
 
+const CACHE_NAME = 'my-pwa-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  // Add your build assets here if not hashed: e.g., '/static/js/main.js'
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activated');
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  console.log('[ServiceWorker] Fetch', event.request.url);
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((cachedResponse) => {
+          return cachedResponse || caches.match('/');
+        });
+      })
+  );
 });
