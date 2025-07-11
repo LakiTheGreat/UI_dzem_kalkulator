50bf5bd9293e044c916a2d72d8304648e78b701c
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-11 13:07:04 +0200

    Fruits form

--- src/pages/NewOrderPage/newForm.tsx
+++ src/pages/NewOrderPage/newForm.tsx
@@ -1,9 +1,8 @@
-import { useForm, useFieldArray } from 'react-hook-form';
-import { useEffect } from 'react';
-import { Button, Container, Stack, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { Button, Container, Divider, Stack } from '@mui/material';
 
 import FormProvider from '../../components/FormProvider';
-import RHFTextInput from '../../components/RHFTextInput';
+import FruitsForm from './from/FruitsForm';
 
 type FruitItem = {
   grams: string | number;
@@ -22,27 +21,7 @@ export default function NewForm() {
     },
   });
 
-  const { control, register, watch, setValue, handleSubmit } = methods;
-
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'fruits',
-  });
-
-  const { fruits } = watch();
-
-  useEffect(() => {
-    console.log(fruits);
-    fruits.forEach((fruit, index) => {
-      const grams = Number(fruit.grams);
-      const price = Number(fruit.price);
-
-      const total =
-        grams > 0 && price > 0 ? ((price / 1000) * grams).toFixed(0) : '';
-
-      setValue(`fruits.${index}.total`, total);
-    });
-  }, [fruits, setValue]);
+  const { handleSubmit } = methods;
 
   const formSubmit = (data: FormData) => {
     console.log('Submitted:', data);
@@ -51,54 +30,11 @@ export default function NewForm() {
   return (
     <Container maxWidth='sm'>
       <FormProvider methods={methods} onSubmit={handleSubmit(formSubmit)}>
-        <Stack spacing={2}>
-          <Typography variant='h6'>Fruits</Typography>
-
-          {fields.map((field, index) => (
-            <Stack
-              direction='row'
-              spacing={2}
-              key={field.id}
-              alignItems='center'
-            >
-              <Stack sx={{ flex: 1 }}>
-                <RHFTextInput
-                  type='number'
-                  label='Grams'
-                  {...register(`fruits.${index}.grams`)}
-                />
-              </Stack>
-
-              <Stack sx={{ flex: 1 }}>
-                <RHFTextInput
-                  type='number'
-                  label='Price/kg'
-                  {...register(`fruits.${index}.price`)}
-                />
-              </Stack>
-              <Stack sx={{ flex: 1 }}>
-                <Typography>{`Total: ${fruits[index].total}`}</Typography>
-              </Stack>
-              <Button
-                variant='outlined'
-                color='error'
-                onClick={() => remove(index)}
-                sx={{ flex: 1 }}
-              >
-                Remove
-              </Button>
-            </Stack>
-          ))}
-
-          <Button
-            variant='outlined'
-            onClick={() => append({ grams: '', price: '', total: '' })}
-          >
-            Add Fruit
-          </Button>
-
-          <Button type='submit' variant='contained'>
-            Submit
+        <Stack gap={4}>
+          <FruitsForm />
+          <Divider />
+          <Button variant='contained' type='submit'>
+            Sačuvaj
           </Button>
         </Stack>
       </FormProvider>
