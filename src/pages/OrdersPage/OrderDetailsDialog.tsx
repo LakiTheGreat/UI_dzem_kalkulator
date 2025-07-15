0a8df62af16259f2d79bb41e71c6525e53c6b0db
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-15 10:21:37 +0200

    polish

--- src/pages/OrdersPage/OrderDetailsDialog.tsx
+++ src/pages/OrdersPage/OrderDetailsDialog.tsx
@@ -1,6 +1,9 @@
-import { Skeleton } from '@mui/material';
+import { Skeleton, Stack } from '@mui/material';
 import { useGetOrderByIdQuery } from '../../api/ordersApi';
 import GeneralDialog from '../../components/GeneralDialog';
+import { Typography } from '@mui/material';
+import { ORDER_WIDTH } from '../../constants';
+import { formatLocalDateTime } from '../../utils/formatLocalDateTime';
 
 type Props = {
   open: boolean;
@@ -23,7 +26,22 @@ export default function OrderDetailsDialog({
       title={'Detalji porudžbine'}
     >
       {isLoading && <Skeleton variant='rounded' height={300} />}
-      {!isLoading && data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {!isLoading && data && (
+        <Stack>
+          <Stack direction='row' gap={1}>
+            <Typography sx={{ width: ORDER_WIDTH }}>Napomena:</Typography>
+            <Typography>{data.orderName}</Typography>
+          </Stack>
+          <Stack direction='row' gap={1}>
+            <Typography sx={{ width: ORDER_WIDTH }}>Vreme unosa:</Typography>
+            <Typography>{formatLocalDateTime(data.createdAt)}</Typography>
+          </Stack>
+          {/* <Stack direction='row' gap={1}>
+            <Typography sx={{ width: ORDER_WIDTH }}>Vrsta džema:</Typography>
+            <Typography>{data.orderType.label}</Typography> 
+          </Stack> */}
+        </Stack>
+      )}
     </GeneralDialog>
   );
 }
